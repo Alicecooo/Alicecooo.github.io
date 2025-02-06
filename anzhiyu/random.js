da24f8f4a66c8868f14ed6651a377664b5612ff6
Alicecooo <1169946225@qq.com>
2025-02-06 15:54:51 +0800

    Site updated: 2025-02-06 15:54:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/06/Game Engine/","2025/02/04/CS106B笔记/","2025/02/06/Threadpool/","2025/02/04/大一上总结/"];function toRandomPost(){
+var posts=["2025/02/06/Game Engine/","2025/02/06/Threadpool/","2025/02/04/CS106B笔记/","2025/02/04/大一上总结/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
